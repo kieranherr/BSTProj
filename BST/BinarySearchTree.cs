58408963cb27cee2dfbd5ef6eb3553f746d7f6e6
Kieran <kieranherr@gmail.com>
2020-04-08 08:04:16 -0500

    Forgot to make repo

---
+++ BST/BinarySearchTree.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BST
+{
+    public class BinarySearchTree
+    {
+
+        
+         public class Node
+        {
+            public int data;
+            public Node leftChild;
+            public Node rightChild;
+        }
+        public Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        
+        public void AddToTree(int add)
+        {
+            Node newNode = new Node();
+            newNode.data = add;
+            if(root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node temp = root;
+                Node parent;
+                while (true)
+                {
+                    parent = temp;
+                    if(add < temp.data)
+                    {
+                        temp = temp.leftChild;
+                        if(temp == null)
+                        {
+                            parent.leftChild = newNode;
+                            break;
+                        }
+                        else
+                        {
+                            temp = temp.rightChild;
+                            if(temp == null)
+                            {
+                                parent.rightChild = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public bool Find(int value)
+        {
+            if(root == null)
+            {
+                return false;
+            }
+            else if(value < root.data)
+            {
+                
+                return Find(value);
+            }
+            else
+        }
+
+    }
+}
+
---
+++ BST/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree newTree = new BinarySearchTree();
+            newTree.AddToTree(45);
+            newTree.AddToTree(50);
+            newTree.AddToTree(2);
+            newTree.AddToTree(15);
+            newTree.AddToTree(80);
+        }
+    }
+}
